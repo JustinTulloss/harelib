bf5b0a90694e785b5974d8fbc9ba0c43d1f3c6ba
Justin Tulloss <jmtulloss@gmail.com>
2009-11-18 23:42:06 -0800

    Added epoll for python 2.5 on linux, borrowed from tornado

--- setup.py
+++ setup.py
@@ -9,11 +9,20 @@ Python Distutils setup for for amqp.  Build and install with
 """
 
 import sys
-from distutils.core import setup
+from distutils.core import setup, Extension
+
+# Build the epoll extension for Linux systems with Python < 2.6
+extensions = []
+major, minor = sys.version_info[:2]
+python_26 = (major > 2 or (major == 2 and minor >= 6))
+if "linux" in sys.platform.lower() and not python_26:
+    extensions.append(Extension(
+        "amqplib.client_0_8.epoll", ["amqplib/client_0_8/epoll.c"]))
 
 setup(name = "amqplib",
       description = "AMQP Client Library",
       version = "0.6.1",
+      ext_modules = extensions,
       license = "LGPL",
       author = "Barry Pederson",
       author_email = "bp@barryp.org",
